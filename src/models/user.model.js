b744b053fb1b30428f93bd5e407cc257f6cd586a
Shreyasi53 <shreyasidutta53@gmail.com>
2026-05-12 11:19:07 +0530

    fix all register controller bugs

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import bcryptjs from 'bcryptjs';
 
 const userSchema = new Schema(
     {
@@ -19,7 +19,7 @@ const userSchema = new Schema(
             lowercase: true,
             trim: true,
         },
-        fullName:{
+        fullname:{
             type: String,
             required: true,
             trim: true,
@@ -51,17 +51,14 @@ const userSchema = new Schema(
     }
 );
 
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        return next();
-    }
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
 
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
+    this.password = await bcryptjs.hash(this.password, 10);
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-    return await bcrypt.compare(password, this.password);
+    return await bcryptjs.compare(password, this.password);
 };
 
 userSchema.methods.generateAccessToken = function () {
@@ -70,7 +67,7 @@ userSchema.methods.generateAccessToken = function () {
             _id: this._id ,
             email: this.email,
             username: this.username,
-            fullname: this.fullName,
+            fullname: this.fullname,
         },   
         process.env.ACCESS_TOKEN_SECRET,
         {
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,7 +16,8 @@ const uploadOnCloudinary = async (loacalFilePath) => {
             resource_type: "auto"
         })
         //file has been uploaded successfully
-        console.log("file is uploaded on cloudinary", response.url);
+        // console.log("file is uploaded on cloudinary", response.url);
+        fs.unlinkSync(loacalFilePath) //remove the locally saved temporary file
         return response;
     }catch(error){
         fs.unlinkSync(loacalFilePath) //remove the locally saved temporary file as the upload operation got failed
